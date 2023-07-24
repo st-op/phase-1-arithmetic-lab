8c2e162c47d588da2aa3e7b54829c1c474cb6d11
Stella Johansson <stellajoh9@gmail.com>
2023-07-24 11:09:29 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+let num1 = 31
+let num2 = 2
+let num3 = 22
+let num4 = 6
+let set = (0, 2, 8, 19, 20)
 
-// Write your code here
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+// console.log(multiply(num1, num2))
+
+function random() {
+    return Math.random
+}
+
+// let random = math.random()
+// mod = num3 % num4
+// let max 
+
+function mod(num3, num4) {
+    return num3 % num4
+}
+// console.log(mod(num3, num4))
+
+function max(set) {
+    return Math.max(set)
+}
+console.log(max(set))
+
